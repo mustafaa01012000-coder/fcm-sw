05a16e98a619f2886d7e9074dd9a8a5b1c6afc43
mustafaa01012000-coder <mustafaa01012000@gmail.com>
2026-06-08 19:39:36 +0300

    Add Firebase messaging service worker file

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,12 @@
+importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "AIzaSyBLvMs1wPf_IbkMHXcXD1gGCTOxzaDezVs",
+  authDomain: "i3lamk.firebaseapp.com",
+  projectId: "i3lamk",
+  messagingSenderId: "257867573368",
+  appId: "1:257867573368:web:ac4baaa3fd86012140258d"
+});
+
+const messaging = firebase.messaging();
